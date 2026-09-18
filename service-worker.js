d2ca097b8967f92c12f65f3e0e1a3b8a2385ed4d
rianioussama <riani.oussama@gmail.com>
2026-09-18 17:04:58 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,52 +1,58 @@
-const CACHE_NAME =
- "courses-v1";
+const CACHE_NAME = "courses-v2";
 
 const urlsToCache = [
-
- "./",
- "./index.html",
- "./manifest.json"
-
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-self.addEventListener(
- "install",
- event=>{
+self.addEventListener("install", event => {
 
   event.waitUntil(
 
-   caches.open(CACHE_NAME)
-    .then(cache=>{
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
 
-      return cache.addAll(
-       urlsToCache
+  );
+
+});
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+
+    caches.keys().then(keys => {
+
+      return Promise.all(
+
+        keys.map(key => {
+
+          if (key !== CACHE_NAME) {
+
+            return caches.delete(key);
+
+          }
+
+        })
+
       );
 
     })
 
   );
 
- });
+});
 
-self.addEventListener(
- "fetch",
- event=>{
+self.addEventListener("fetch", event => {
 
   event.respondWith(
 
-   caches.match(
-    event.request
-   )
-
-   .then(response=>{
-
-    return response ||
-           fetch(
-            event.request
-           );
-
-   })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
 
   );
 
- });
+});
+``
