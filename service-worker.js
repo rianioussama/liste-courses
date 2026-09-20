6d908f17fa385476ecaff1f2df3fd7bb3be5c8b2
rianioussama <riani.oussama@gmail.com>
2026-09-20 23:21:37 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -55,26 +55,32 @@ self.addEventListener("fetch", event => {
 
   event.respondWith(
 
-    fetch(event.request)
+   fetch(event.request)
+  .then(response => {
 
-      .then(response => {
+    if(
+      event.request.method === 'GET' &&
+      response.status === 200
+    ){
 
-        const responseClone =
-          response.clone();
+      const responseClone =
+        response.clone();
 
-        caches.open(CACHE_NAME)
-          .then(cache => {
+      caches.open(CACHE_NAME)
+        .then(cache => {
 
-            cache.put(
-              event.request,
-              responseClone
-            );
+          cache.put(
+            event.request,
+            responseClone
+          );
 
-          });
+        });
 
-        return response;
+    }
 
-      })
+    return response;
+
+  }))
 
       .catch(() => {
 
